74b0d9a5b22fd96f6d2ea878af78c35fcfe6bf15
master8lcm8-maker <master8lcm8@gmail.com>
2025-11-25 02:00:54 -0500

    Update auth.controller.ts

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,8 +1,13 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { IsEmail, IsString, MinLength } from 'class-validator';
 
 class LoginDto {
+  @IsEmail()
   email: string;
+
+  @IsString()
+  @MinLength(4)
   password: string;
 }
 
@@ -12,10 +17,12 @@ export class AuthController {
 
   @Post('login')
   async login(@Body() loginDto: LoginDto) {
+    // validateUser will throw UnauthorizedException if credentials are wrong
     const user = await this.authService.validateUser(
       loginDto.email,
       loginDto.password,
     );
+
     return this.authService.login(user);
   }
 }
