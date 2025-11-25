84a355d94bc87bd26a9eccb7cbd3106cd4d32dc9
master8lcm8-maker <master8lcm8@gmail.com>
2025-11-24 21:33:42 -0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,15 @@ import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Allow requests from your app (for now, open to all; we can tighten later)
+  app.enableCors({
+    origin: '*',
+  });
+
+  // Optional but recommended: prefix all routes with /api
+  app.setGlobalPrefix('api');
+
   await app.listen(process.env.PORT || 8080);
 }
 
