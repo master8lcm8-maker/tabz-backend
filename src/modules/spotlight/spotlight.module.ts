1d4b38380c02588f0d00392918156acbcdbf6f22
master8lcm8-maker <master8lcm8@gmail.com>
2025-11-25 05:00:45 -0500

    Update spotlight.module.ts

--- src/modules/spotlight/spotlight.module.ts
+++ src/modules/spotlight/spotlight.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
+import { SpotlightController } from './spotlight.controller';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
-  exports: [],
+  controllers: [SpotlightController],
 })
 export class SpotlightModule {}
