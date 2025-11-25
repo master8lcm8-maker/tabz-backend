73c3b3c7a0f97580422aba44184b40d40d19ec90
master8lcm8-maker <master8lcm8@gmail.com>
2025-11-25 03:43:11 -0500

    Update users.controller.ts

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
 @Controller('users')
 export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @UseGuards(JwtAuthGuard)
   @Get()
   getUsers() {
-    return { message: 'Users endpoint working' };
+    return this.usersService.getUsers();
   }
 }
