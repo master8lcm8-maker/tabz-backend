f4877a46e023a1e81b01c8cda23f518a0a84274b
master8lcm8-maker <master8lcm8@gmail.com>
2025-12-02 04:07:24 -0500

    Update venue-wallet.entity.ts

--- src/wallet/venue-wallet.entity.ts
+++ src/wallet/venue-wallet.entity.ts
@@ -1,23 +1,33 @@
+// src/wallet/venue-wallet.entity.ts
 import {
   Entity,
   PrimaryGeneratedColumn,
   Column,
+  OneToMany,
   CreateDateColumn,
   UpdateDateColumn,
 } from 'typeorm';
+import { VenueWalletTransaction } from './venue-wallet-transaction.entity';
 
 @Entity('venue_wallets')
 export class VenueWallet {
   @PrimaryGeneratedColumn()
   id: number;
 
-  // ID of the venue (from your venues table / system)
   @Column()
   venueId: number;
 
-  // Total earnings for this venue in cents
   @Column({ type: 'bigint', default: 0 })
   balanceCents: number;
+
+  @Column({ type: 'bigint', default: 0 })
+  cashoutAvailableCents: number;
+
+  @OneToMany(
+    () => VenueWalletTransaction,
+    (tx) => tx.venueWallet,
+  )
+  transactions: VenueWalletTransaction[];
 
   @CreateDateColumn()
   createdAt: Date;
