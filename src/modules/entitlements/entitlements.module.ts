27cb63a67ec2c5c653230783b07f2ab269f3df07
Troy <master8lcm8@gmail.com>
2026-05-31 17:08:36 -0400

    Phase 26E: add entitlements module

---
+++ src/modules/entitlements/entitlements.module.ts
@@ -0,0 +1,23 @@
+﻿import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { DrinkOrder } from '../drinks/entities/drink-order.entity';
+import { FreeboardDrop } from '../freeboard/freeboard-drop.entity';
+import { FoodOrder } from '../foods/entities/food-order.entity';
+
+import { EntitlementsController } from './entitlements.controller';
+import { EntitlementsService } from './entitlements.service';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      DrinkOrder,
+      FreeboardDrop,
+      FoodOrder,
+    ]),
+  ],
+  controllers: [EntitlementsController],
+  providers: [EntitlementsService],
+  exports: [EntitlementsService],
+})
+export class EntitlementsModule {}
