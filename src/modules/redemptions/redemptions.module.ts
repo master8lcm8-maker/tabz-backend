dafd67beccf2612adbd007ce0050ad2ac1a02673
Troy <master8lcm8@gmail.com>
2026-05-31 17:23:45 -0400

    Phase 26E: add generic redemptions module

---
+++ src/modules/redemptions/redemptions.module.ts
@@ -0,0 +1,18 @@
+﻿import { Module } from '@nestjs/common';
+
+import { FoodsModule } from '../foods/foods.module';
+import { QrModule } from '../qr/qr.module';
+
+import { RedemptionsController } from './redemptions.controller';
+import { RedemptionsService } from './redemptions.service';
+
+@Module({
+  imports: [
+    QrModule,
+    FoodsModule,
+  ],
+  controllers: [RedemptionsController],
+  providers: [RedemptionsService],
+  exports: [RedemptionsService],
+})
+export class RedemptionsModule {}
